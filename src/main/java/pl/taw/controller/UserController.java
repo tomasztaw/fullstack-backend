585a92d7b6a3ff61bd51f27c5f64f687ce6789df
tomasztaw <tomasz.wegrzyn.wro@gmail.com>
2023-10-14 18:18:20 +0200

    basic working application

--- src/main/java/pl/taw/controller/UserController.java
+++ src/main/java/pl/taw/controller/UserController.java
@@ -1,16 +1,15 @@
 package pl.taw.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.taw.exception.UserNotFoundException;
 import pl.taw.model.User;
 import pl.taw.repository.UserRepository;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
@@ -24,5 +23,32 @@ public class UserController {
     @GetMapping("/users")
     List<User> getAllUser() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@PathVariable Long id, @RequestBody User newUser) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setName(newUser.getName());
+                    user.setUsername(newUser.getUsername());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                })
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User with id %s has been deleted success.".formatted(id);
     }
 }
